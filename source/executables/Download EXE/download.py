987851ebee09832001474801e4ce4252220afe76
SagiNimni <nimni.sagi@gmail.com>
2020-01-24 14:05:47 +0200

    added error control to download.exe

--- source/executables/Download EXE/download.py
+++ source/executables/Download EXE/download.py
@@ -3,6 +3,8 @@ import sys
 import os
 import shutil
 import ntpath
+import tkinter
+from tkinter import messagebox
 from executables.enums import Cloud
 from WrapperAPI.CloudsAPI import GoogleDriveCloud, MegaUploadCloud, DropboxCloud
 from executables.utillty import handle_remove_readonly
@@ -14,23 +16,30 @@ def main(directory):
     if os.path.exists(prop_path):
         config.read(prop_path)
         properties = {'type': config['DriveSettings']['type'], 'path': config['DriveSettings']['path']}
-        if properties['type'] == Cloud.GoogleDrive.value:
-            shutil.rmtree(directory, ignore_errors=False, onerror=handle_remove_readonly)
-            directory, tail = ntpath.split(directory)
-            service = GoogleDriveCloud()
-            service.download_file(properties['path'], directory + '\\')
-        elif properties['type'] == Cloud.MegaUpload.value:
-            shutil.rmtree(directory, ignore_errors=False, onerror=handle_remove_readonly)
-            directory, tail = ntpath.split(directory)
-            service = MegaUploadCloud("nimni.project@gmail.com", "0522724447")
-            service.download(properties['path'], directory)
-        elif properties['type'] == Cloud.Dropbox.value:
-            shutil.rmtree(directory, ignore_errors=False, onerror=handle_remove_readonly)
-            directory, tail = ntpath.split(directory)
-            service = DropboxCloud("q8AOvG028RAAAAAAAAAARl4cbDhkbW1k0CX9w09-9zce7Aoheti6kRSqXiOaFfeU")
-            service.download_file(properties['path'], directory)
+        try:
+            if properties['type'] == Cloud.GoogleDrive.value:
+                shutil.rmtree(directory, ignore_errors=False, onerror=handle_remove_readonly)
+                directory, tail = ntpath.split(directory)
+                service = GoogleDriveCloud()
+                service.download_file(properties['path'], directory + '\\')
+            elif properties['type'] == Cloud.MegaUpload.value:
+                shutil.rmtree(directory, ignore_errors=False, onerror=handle_remove_readonly)
+                directory, tail = ntpath.split(directory)
+                service = MegaUploadCloud("nimni.project@gmail.com", "0522724447")
+                service.download(properties['path'], directory)
+            elif properties['type'] == Cloud.Dropbox.value:
+                shutil.rmtree(directory, ignore_errors=False, onerror=handle_remove_readonly)
+                directory, tail = ntpath.split(directory)
+                service = DropboxCloud("q8AOvG028RAAAAAAAAAARl4cbDhkbW1k0CX9w09-9zce7Aoheti6kRSqXiOaFfeU")
+                service.download_file(properties['path'], directory)
+        except Exception as e:
+            root = tkinter.Tk()
+            root.withdraw()
+            messagebox.showerror("Error", e)
     else:
-        raise NotADirectoryError("This directory does not exist, or has been removed")
+        root = tkinter.Tk()
+        root.withdraw()
+        messagebox.showerror("Error", "The prop file is missing.")
 
 
 if __name__ == '__main__':
